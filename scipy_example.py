fed54ca7330944f52d377714540a22df64a9635c
BabyOstrich <Camille Avestruz>
2016-06-02 14:36:18 -0500

    added the plt show

--- scipy_example.py
+++ scipy_example.py
@@ -2,13 +2,18 @@
 plot the results'''
 
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 def func(x, a, b, c):
     '''This returns an array from performing e^?? on 
     x where x is the element by element array.  a, b, 
     c are just constants of the function.'''
-    return a * np.exp(-b * x) + cxdata = np.linspace(0, 4, 50)
+    
+    return a * np.exp(-b * x) + c 
+
+
+xdata = np.linspace(0, 4, 50)
 y = func(xdata, 2.5, 1.3, 0.5)
 ydata = y + 0.2 * np.random.normal(size=len(xdata))
 
@@ -28,3 +33,5 @@ plt.plot(xdata,y,color='g')
 plt.plot(xdata,ydata,marker="*",color='m')
 # best fit y data to the perturbed data
 plt.plot(xdata,func(xdata,popt[0],popt[1],popt[2]),marker='o',color='r')
+
+plt.show()
