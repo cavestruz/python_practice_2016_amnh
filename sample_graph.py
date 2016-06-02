82868e7f4bcd52e0e3abbef345c400833b1bd03b
BabyOstrich <Camille Avestruz>
2016-06-02 12:57:34 -0500

    Added a sample graph

---
+++ sample_graph.py
@@ -0,0 +1,9 @@
+'''This makes our first sample plot'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+mydata = np.loadtxt('data.txt')
+x = mydata[:,0]
+y = mydata[:,1]
+line = plt.plot(x,y)
+plt.show()
